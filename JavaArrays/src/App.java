61df3e6a3a42ba102c5a5495d9298e74c43a6488
wesleynickchen <wesley.nickchen@gmail.com>
2021-11-09 09:55:52 -0600

    .

--- JavaArrays/src/App.java
+++ JavaArrays/src/App.java
@@ -1,5 +1,26 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int keyAge[] = {0,5,10,13,15,16,18,21,30,40,50,55,65,70,100};
+        String keyEvent[] = {"Birth","Kindergarten","Preteen","Teen","Driving","Sweet16",
+        "High School Graduation","Adult-All States", "Thirties","Forties","Fifties","Early Retirement","Retirement",
+        "Federal Retirement", "Centurian"};
+
+        //keyAgeToYear(keyAge);
+        fullReport(keyAgeToYear(keyAge), keyEvent);
+        
+        
+
+    }
+    public static int[] keyAgeToYear(int[] key) {
+        int key2[] = new int[key.length];
+        for(int i = 0; i < key.length; i++){
+            key2[i] = 1903 + key[i];
+        }
+        return key2;
+    }
+    private static void fullReport(int[] key, String[] keys) {
+        for(int i = 0; i < key.length; i++){
+            System.out.println(key[i] + "\t" + keys[i]);
+        }
     }
 }
